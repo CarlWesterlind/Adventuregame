3f1bedaa6532954dae21f16920162c14909a555c
Salem Lokende <salo0002@stud.ek.dk>
2026-09-23 11:39:33 +0200

    Created a Map class Did part of the method, buildMap

---
+++ src/Uge39_Adventure/Map.java
@@ -0,0 +1,25 @@
+package Uge39_Adventure;
+
+public class Map {
+
+    public static void buildMap()
+        {
+
+            Room room1 = new Room("Room 1", "An empty room");
+            Room room2 = new Room("Room 2", "A bright room");
+            Room room3 = new Room("Room 3", "A dark room");
+            Room room4 = new Room("Room 4", "A room that smells bad");
+            Room room5 = new Room("Room 5", "A room that full of trees");
+            Room room6 = new Room("Room 6", "A very dark room");
+            Room room7 = new Room("Room 7", "A room full of very bright colours");
+            Room room8 = new Room("Room 8", "A fun room");
+            Room room9 = new Room("Room 9", "A terrible room");
+
+
+
+
+        }
+
+        connectEastWest
+
+}
