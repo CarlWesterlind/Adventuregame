7d23d0d84e57a1eca0c4e8666c7a89dcd5c11eec
Salem Lokende <salo0002@stud.ek.dk>
2026-09-24 09:51:42 +0200

    Changed Map setNorths

--- src/Uge39_Adventure/Map.java
+++ src/Uge39_Adventure/Map.java
@@ -22,7 +22,7 @@ public class Map {
             room2.setWest(room1);
 
             room3.setSouth(room6);
-            room3.setEast(room2);
+            room3.setWest(room2);
 
             room4.setNorth(room1);
             room4.setSouth(room7);
@@ -32,7 +32,7 @@ public class Map {
             room6.setNorth(room3);
             room6.setSouth(room9);
 
-            room7.setNorth(room7);
+            room7.setNorth(room4);
             room7.setEast(room8);
 
             room8.setNorth(room5);
