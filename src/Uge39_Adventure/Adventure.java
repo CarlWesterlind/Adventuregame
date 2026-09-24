235163adc2ca8c2a5b63c5729bbe6fb35784a395
Salem Lokende <salo0002@stud.ek.dk>
2026-09-24 10:54:22 +0200

    Made a getter and setter for currentRoom in the Map class

--- src/Uge39_Adventure/Adventure.java
+++ src/Uge39_Adventure/Adventure.java
@@ -3,4 +3,6 @@ package Uge39_Adventure;
 public class Adventure {
 
 
+
+
 }
--- src/Uge39_Adventure/Map.java
+++ src/Uge39_Adventure/Map.java
@@ -3,6 +3,14 @@ package Uge39_Adventure;
 public class Map {
 
 
+        private static Room currentRoom;
+
+        public static Room getCurrentRoom(){
+                return currentRoom;
+        }
+        public static void setCurrentRoom(Room room){
+                currentRoom = room;
+        }
 
     public static Room buildMap() {
 
