a6f11bfef9cd97202e7a5d856d26fe2af59b787c
MathiasSand <Sand.mathias@icloud.com>
2026-09-25 14:22:49 +0200

    Sat så spillet kan genkende hvilken run vi starter i.

--- src/Uge39_Adventure/UserInterface.java
+++ src/Uge39_Adventure/UserInterface.java
@@ -10,7 +10,7 @@ public class UserInterface {
         boolean running = true;
         System.out.print("Welcome to Adventure!\n");
         System.out.print("Type 'help' for commands\n");
-        System.out.print("You are in room 1\n");
+        System.out.print(adventure.look());
 
         while(running){
             System.out.print("\n > ");
